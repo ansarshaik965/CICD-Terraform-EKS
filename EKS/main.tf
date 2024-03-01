6539fd2cca6f550181453201bebf529cfe7152f1
ansarshaik <ansarshaik965@gmail.com>
2024-03-01 17:41:46 +0530

    change user

--- EKS/main.tf
+++ EKS/main.tf
@@ -34,7 +34,7 @@ module "vpc" {
 module "eks" {
   source                         = "terraform-aws-modules/eks/aws"
   cluster_name                   = "my-eks-cluster"
-  cluster_version                = "1.24"
+  cluster_version                = "1.29"
   cluster_endpoint_public_access = true
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
