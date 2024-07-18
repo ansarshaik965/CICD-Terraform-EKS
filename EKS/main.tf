8714dd948e9705bc519b24da5fed1e0cb0b1dfc1
ANSAR SHAIK <145973205+ansarshaik965@users.noreply.github.com>
2024-07-18 13:19:23 +0530

    Update main.tf

--- EKS/main.tf
+++ EKS/main.tf
@@ -52,3 +52,11 @@ module "eks" {
     Terraform   = "true"
   }
 }
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
